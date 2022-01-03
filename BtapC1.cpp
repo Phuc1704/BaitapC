0b02b041507b8e6fd535fe414f0f8decbd287bb9
Phuc1704 <lethanhphucsentos@gmail.com>
2022-01-03 10:56:50 +0700

    first commit

--- BtapC1.cpp
+++ BtapC1.cpp
@@ -1,22 +1,22 @@
-#include<stdio.h>
+#include<stdio.h> //Btap1 Tinh tong cac so hang
 #include<math.h>
 int sum(int n);
-
 int main()
 {
-    int n;
-    float tong;
+    int n, tong;
     printf("nhap n: ");
     scanf("%d", &n);
     tong=sum(n);
-    printf("tong cac so: %.2f", tong);
-    }
+    printf("tong cac so: %d", tong);
+    
+}
 int sum(int n)
 {
-    float sum1=0, i;
+    int sum1=0, i;
     for(i=0; i<=n; i++)
     {
-        sum1+=i/(2*n);
+        sum1+=i;
     }
     return sum1;
 }
+
