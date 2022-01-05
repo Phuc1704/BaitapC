6576b41dedc754664afcc59edb0163ecf6bb111e
Phuc1704 <lethanhphucsentos@gmail.com>
2022-01-05 08:53:13 +0700

    first commit

---
+++ BtapC6.cpp
@@ -0,0 +1,85 @@
+//Btap 6
+#include<stdio.h>
+int main ()
+{
+    int n; 
+    printf("nhap n: ");
+    scanf("%d", &n);
+    float sum=0, i;
+    for(i=1; i<=n; i++)
+    {
+        sum+=1.0/(i*(i+1));
+    }
+    printf("tong cac so: %.2f", sum);
+    return 0;
+}
+
+//Btap 7
+/*#include<stdio.h>
+int main ()
+{
+    int n; 
+    printf("nhap n: ");
+    scanf("%d", &n);
+    float sum=0, i;
+    for(i=1; i<=n; i++)
+    {
+        sum+=i/(i+1);
+    }
+    printf("tong cac so: %f", sum);
+    return 0;
+}
+
+//btap8
+#include<stdio.h>
+int main ()
+{
+    int n; 
+    printf("nhap n: ");
+    scanf("%d", &n);
+    float sum=0, i;
+    for(i=0; i<n; i++)
+    {
+        sum+=(2*i+1)/(2*i+2);
+    }
+    printf("tong cac so: %f", sum);
+    return 0;
+}
+
+//Btap 9
+#include<stdio.h>
+int main ()
+{
+    int n; 
+    printf("nhap n: ");
+    scanf("%d", &n);
+    float tich=1, i;
+    for(i=1; i<=n; i++)
+    {
+        tich*=i;
+    }
+    printf("tich cac so: %.2f", tich);
+    return 0;
+}
+
+//Btap 10
+#include <stdio.h>
+int main()
+{
+int i,x,n;
+printf("nhap so x :");
+scanf("%d",&x);
+printf("nhap so n :");
+scanf("%d",&n);
+int ketqua =1;
+for(i=1;i<=n;i++)
+{
+ ketqua*=x;
+}
+printf("ket qua x^n : = %d",ketqua);
+return 0;
+}*/
+
+
+
+
